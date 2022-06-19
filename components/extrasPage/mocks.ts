a2d02fc8b7c31c15e9d0b138edd1b5fda357ca1a
uyohn <rastockymatej@gmail.com>
2022-06-19 12:56:49 +0300

    final

--- components/extrasPage/mocks.ts
+++ components/extrasPage/mocks.ts
@@ -1,12 +1,14 @@
 export const SMART_DATA = {
-  summary: 'Lorem ipsum dolor sit amet...',
+  summary: 'Biggest down right now is that we have different parts of code on different computers and we need to bring it all together and streamline it continuous process.Yes, and the automatic downloading is kind of where we are lacking because we cannot users to create API keys, connect to it.We will not ask for Microsoft Login or we will just for OneDrive link.Then that downloaded file will be converted into the audio file.',
   dateTimeEntities: [
-    { tomorrow: ['bullet point 1', 'bullet point 2'] },
-    { 'next week': ['bullet point 1', 'bullet point 2'] }
+    {'a second': ['[en-US] Room for a second. It\'s OK if it\'s all right.']},
+    {'today': ['[en-US] What are our meeting purposes for today?']},
+    {'right now': ['[en-US] Biggest down right now is that we have different parts of code on different computers and we need to bring it all together and streamline it continuous process.']},
+    {'next week': ['[en-US] That will spew out some keywords. It will summarize it will find the event locations. So for example, one event we will be doing is next week, Wednesday at 3:00 o\'clock. We will have another meeting about this app and how we can make it a successful product. Ready to go to market.,[en-US] And the task for Carl next week. Next week\'s meeting is that he prepares more jokes.']},
+    {'few minutes': ['[en-US] I agree that the first few minutes are quite this was, I guess, but you said see that as well. I guess we should vote watch the meeting. Productive or not. Yes it was.']},
   ],
   eventEntities: [
-    { presentation: ['presentation 1', 'presentation 2'] },
-    { 'something else': ['something else 1', 'something else 2'] }
+    { presentation: ['meeting [en-US] To be proceeding the same, yes. OK, so the same link, the same meeting you got OK.,[en-US] So this meeting that always jokes.,[en-US] What are our meeting purposes for today?,[en-US] That will spew out some keywords. It will summarize it will find the event locations. So for example, one event we will be doing is next week, Wednesday at 3:00 o\'clock. We will have another meeting about this app and how we can make it a successful product. Ready to go to market.,[en-US] And the task for Carl next week. Next week\'s meeting is that he prepares more jokes.,[en-US] Anything else, I think this meeting was very productive and I will see I think as well.,[en-US] I agree that the first few minutes are quite this was, I guess, but you said see that as well. I guess we should vote watch the meeting. Productive or not. Yes it was.'] },
   ],
   satisfactionRate: 0.71
 }
